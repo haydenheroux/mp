79b964dd0e607b80a7b0c77b57fc4aab711b64cf
Hayden Heroux <haydenheroux@gmail.com>
2022-08-31 00:36:16 -0400

    ci: Add a define in config.mk for PLAYING_FILE_IS_STDOUT.

--- config.mk
+++ config.mk
@@ -1,5 +1,6 @@
 PREFIX = /usr/local
 
-CFLAGS = -g -std=c99 -pedantic -Wall -Os
+CDEFS = -DPLAYING_FILE_IS_STDOUT
+CFLAGS = -g -std=c99 -pedantic -Wall -Os $(CDEFS)
 LDFLAGS = -lmpg123 -lao
 CC = cc
